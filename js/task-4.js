0102e488523abb60a78ca83559343349c406622e
Illia <mddg354@gmail.com>
2026-01-18 18:58:05 +0200

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -3,8 +3,9 @@ loginForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const email = event.target.elements.email;
   const password = event.target.elements.password;
-  if (!email.value.trim() || !password.value) {
+  if (!email.value.trim() || !password.value.trim()) {
     alert("All form fields must be filled in");
+    return;
   }
   const newObj = { email: email.value.trim(), password: password.value.trim() };
   console.log(newObj);
